f59e49d528da2004e6473a8fc18e147fdd141168
Manovendra Mukherjee <manav2k6@gmail.com>
2018-02-11 23:58:16 +0530

    Add files via upload

---
+++ AppGWSol.ps1
@@ -0,0 +1,5 @@
+﻿$AppGw = Get-AzureRmApplicationGateway -Name "" -ResourceGroupName ""
+$AppGw | Set-AzureRmApplicationGatewayWebApplicationFirewallConfiguration -Enabled $true -FirewallMode Prevention
+Set-AzureRmApplicationGateway -ApplicationGateway $AppGw
+
+#Stop-AzureRmApplicationGateway -ApplicationGateway $AppGw
---
+++ SQLSol.ps1
@@ -0,0 +1,4 @@
+﻿Add-AzureRmAccount
+Set-AzureRmSqlDatabaseAuditing -State Enabled -StorageAccountName azurepoc -ServerName "server-1416380557" -DatabaseName iisweb2 -ResourceGroupName "AzurePOC2RG"
+Set-AzureRmSqlDatabaseThreatDetectionPolicy -ResourceGroupName "AzurePOC2RG" -ServerName "server-1416380557" -DatabaseName iisweb2 -EmailAdmins $true -StorageAccountName azurepoc
+Set-AzureRmSqlDatabase -DatabaseName iisweb2 -ResourceGroupName "AzurePOC2RG" -ServerName "server-1416380557"
