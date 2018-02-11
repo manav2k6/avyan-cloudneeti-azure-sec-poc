e04eb3c24ce9a88d29b0b634a08bcf05e7cd90ba
Manovendra Mukherjee <manav2k6@gmail.com>
2018-02-11 23:59:01 +0530

    Delete AppGWSol.ps1

--- AppGWSol.ps1
+++
@@ -1,5 +0,0 @@
-﻿$AppGw = Get-AzureRmApplicationGateway -Name "" -ResourceGroupName ""
-$AppGw | Set-AzureRmApplicationGatewayWebApplicationFirewallConfiguration -Enabled $true -FirewallMode Prevention
-Set-AzureRmApplicationGateway -ApplicationGateway $AppGw
-
-#Stop-AzureRmApplicationGateway -ApplicationGateway $AppGw
