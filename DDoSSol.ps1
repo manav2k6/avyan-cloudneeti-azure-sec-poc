62f87cc2d24cb80d25665c9a5f51672dd0ee10e2
Manovendra Mukherjee <manav2k6@gmail.com>
2018-02-11 23:59:23 +0530

    Delete DDoSSol.ps1

--- DDoSSol.ps1
+++
@@ -1,4 +0,0 @@
-﻿$vnetProps = (Get-AzureRmResource -ResourceType "Microsoft.Network/virtualNetworks" -ResourceGroup "Contoso-DevOps" -ResourceName "Contoso-DevOps-vnet").Properties
-$vnetProps.enableDdosProtection = $true
-$vnetProps.enableVmProtection = $true
-Set-AzureRmResource -PropertyObject $vnetProps -ResourceGroupName "Contoso-DevOps" -ResourceName "Contoso-DevOps-vnet" -ResourceType Microsoft.Network/virtualNetworks -Force
