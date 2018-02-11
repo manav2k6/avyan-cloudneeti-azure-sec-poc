cd7cd83124f56e9e3d2d84797f809eb0bf117f2b
Manovendra Mukherjee <manav2k6@gmail.com>
2018-02-12 00:01:32 +0530

    Add files via upload

---
+++ AzurePOCSolutions/DDoSSol.ps1
@@ -0,0 +1,4 @@
+﻿$vnetProps = (Get-AzureRmResource -ResourceType "Microsoft.Network/virtualNetworks" -ResourceGroup "Contoso-DevOps" -ResourceName "Contoso-DevOps-vnet").Properties
+$vnetProps.enableDdosProtection = $true
+$vnetProps.enableVmProtection = $true
+Set-AzureRmResource -PropertyObject $vnetProps -ResourceGroupName "Contoso-DevOps" -ResourceName "Contoso-DevOps-vnet" -ResourceType Microsoft.Network/virtualNetworks -Force
