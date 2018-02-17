67d6047f6a97aa52b9354d7a03316d1b9287befa
Manovendra Mukherjee <manav2k6@gmail.com>
2018-02-17 14:20:09 +0530

    Add files via upload

---
+++ Scripts/AppG.ps1
@@ -0,0 +1,91 @@
+﻿# Login to Azure subscription
+Add-AzureRmAccount
+
+# Defines a variable for a dotnet get started web app repository location
+$gitrepo="https://github.com/manav2k6/demowebapp"
+	
+# Unique web app name
+$webappname="mywebapp$(Get-Random)"
+
+# Creates a resource group
+$rg = New-AzureRmResourceGroup -Name AzurePOC2RG -Location Eastus
+
+#Azure SQL Database logical server
+$servername = "server-$(Get-Random)"
+# Set an admin login and password for your database
+# The login information for the server
+$adminlogin = "ServerAdmin"
+$password = "ChangeYourAdminPassword1"
+# The ip address range that you want to allow to access your server - change as appropriate
+$startip = "0.0.0.0"
+$endip = "0.0.0.0"
+# The database name
+$databasename = "mydemoWebAppDB"
+
+#Create an Azure SQL Database logical server
+New-AzureRmSqlServer -ResourceGroupName $rg.ResourceGroupName -ServerName $servername -Location EastUS -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminlogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force)) 
+
+#Configure a server firewall rule
+New-AzureRmSqlServerFirewallRule -ResourceGroupName $rg.ResourceGroupName -ServerName $servername -FirewallRuleName "AllowSome" -StartIpAddress $startip -EndIpAddress $endip 
+
+#Create a database in the server
+New-AzureRmSqlDatabase -ResourceGroupName $rg.ResourceGroupName -ServerName $servername -DatabaseName $databasename 
+
+# Create an App Service plan in Free tier.
+New-AzureRmAppServicePlan -Name $webappname -Location EastUs -ResourceGroupName $rg.ResourceGroupName -Tier Free
+
+# Creates a web app
+$webapp = New-AzureRmWebApp -ResourceGroupName $rg.ResourceGroupName -Name $webappname -Location EastUs -AppServicePlan $webappname
+
+# Configure GitHub deployment from your GitHub repo and deploy once to web app.
+$PropertiesObject = @{
+    repoUrl = "$gitrepo";
+    branch = "master";
+    isManualIntegration = "true";
+}
+Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName $rg.ResourceGroupName -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $webappname/web -ApiVersion 2015-08-01 -Force
+
+# Creates a subnet for the application gateway
+$subnet = New-AzureRmVirtualNetworkSubnetConfig -Name subnet01 -AddressPrefix 10.0.0.0/24
+
+# Creates a vnet for the application gateway
+$vnet = New-AzureRmVirtualNetwork -Name appgwvnet -ResourceGroupName $rg.ResourceGroupName -Location EastUs -AddressPrefix 10.0.0.0/16 -Subnet $subnet
+
+# Retrieve the subnet object for use later
+$subnet=$vnet.Subnets[0]
+
+# Create a public IP address
+$publicip = New-AzureRmPublicIpAddress -ResourceGroupName $rg.ResourceGroupName -name publicIP01 -location EastUs -AllocationMethod Dynamic
+
+# Create a new IP configuration
+$gipconfig = New-AzureRmApplicationGatewayIPConfiguration -Name gatewayIP01 -Subnet $subnet
+
+# Create a backend pool with the hostname of the web app
+$pool = New-AzureRmApplicationGatewayBackendAddressPool -Name appGatewayBackendPool -BackendFqdns $webapp.HostNames
+
+# Define the status codes to match for the probe
+$match = New-AzureRmApplicationGatewayProbeHealthResponseMatch -StatusCode 200-399
+
+# Create a probe with the PickHostNameFromBackendHttpSettings switch for web apps
+$probeconfig = New-AzureRmApplicationGatewayProbeConfig -name webappprobe -Protocol Http -Path / -Interval 30 -Timeout 120 -UnhealthyThreshold 3 -PickHostNameFromBackendHttpSettings -Match $match
+
+# Define the backend http settings
+$poolSetting = New-AzureRmApplicationGatewayBackendHttpSettings -Name appGatewayBackendHttpSettings -Port 80 -Protocol Http -CookieBasedAffinity Disabled -RequestTimeout 120 -PickHostNameFromBackendAddress -Probe $probeconfig
+
+# Create a new front-end port
+$fp = New-AzureRmApplicationGatewayFrontendPort -Name frontendport01  -Port 80
+
+# Create a new front end IP configuration
+$fipconfig = New-AzureRmApplicationGatewayFrontendIPConfig -Name fipconfig01 -PublicIPAddress $publicip
+
+# Create a new listener using the front-end ip configuration and port created earlier
+$listener = New-AzureRmApplicationGatewayHttpListener -Name listener01 -Protocol Http -FrontendIPConfiguration $fipconfig -FrontendPort $fp
+
+# Create a new rule
+$rule = New-AzureRmApplicationGatewayRequestRoutingRule -Name rule01 -RuleType Basic -BackendHttpSettings $poolSetting -HttpListener $listener -BackendAddressPool $pool 
+
+# Define the application gateway SKU to use
+$sku = New-AzureRmApplicationGatewaySku -Name Standard_Small -Tier Standard -Capacity 2
+
+# Create the application gateway
+$appgw = New-AzureRmApplicationGateway -Name ContosoAppGateway -ResourceGroupName $rg.ResourceGroupName -Location EastUs -BackendAddressPools $pool -BackendHttpSettingsCollection $poolSetting -Probes $probeconfig -FrontendIpConfigurations $fipconfig  -GatewayIpConfigurations $gipconfig -FrontendPorts $fp -HttpListeners $listener -RequestRoutingRules $rule -Sku $sku
