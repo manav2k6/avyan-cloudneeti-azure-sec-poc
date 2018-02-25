4a85a68f3aa7c702c7ff95e5f752a7b9915f1e44
Manovendra Mukherjee <manav2k6@gmail.com>
2018-02-25 19:50:48 +0530

    Added Switches and made single deployment

--- Scripts/Scenario1.ps1
+++ Scripts/Scenario1.ps1
@@ -1,17 +1,31 @@
 #Requires -Version 3.0
 
 Param(
-     [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
-     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation
-     #[string] [Parameter(Mandatory=$true)] $VMName
-   )
-$TemplateFile = "c:\poc2\templates\Scenario1.json"
-$TemplateFile1parameter = "c:\poc2\templates\IP-Param.json"
+     [string] [Parameter(Mandatory=$true)] $Command,
+     [string] [Parameter(Mandatory=$true)] $DeploymentPrefix
+     )
+$ResourceGroupName= $DeploymentPrefix+'-'+'RG'
+$ResourceGroupLocation= "Southeast Asia"
+$TemplateFile = "..\templates\Scenario1.json"
+$TemplateFile1parameter = "..\templates\IP-Param.json"
 
+switch($Command)
+{
+   Deploy
+	{
 
-# Create or update the resource group using the specified template file and template parameters file
+# Create or update the resource group with name Scenario1 at location southeast asia.
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force
 
+# Deploying infrastructure
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -Name "Deployment" -TemplateParameterFile $TemplateFile1parameter -Verbose
-
-                                       
+     }
+   Metigate
+     {
+$VnetName = "test-vnet"
+$vnetProps = (Get-AzureRmResource -ResourceType "Microsoft.Network/virtualNetworks" -ResourceGroup "$ResourceGroupName" -ResourceName "$VnetName").Properties
+$vnetProps.enableDdosProtection = $true
+$vnetProps.enableVmProtection = $true
+Set-AzureRmResource -PropertyObject $vnetProps -ResourceGroupName "$ResourceGroupName" -ResourceName "$VnetName" -ResourceType "Microsoft.Network/virtualNetworks" -Force
+     }
+}
