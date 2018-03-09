64221bdced0385bb16ba8f14b99fa7e9a8a673d3
Manovendra Mukherjee <manav2k6@gmail.com>
2018-03-09 12:58:36 +0530

    Delete SQLSol.ps1

--- AzurePOCSolutions/SQLSol.ps1
+++
@@ -1,4 +0,0 @@
-﻿Add-AzureRmAccount
-Set-AzureRmSqlDatabaseAuditing -State Enabled -StorageAccountName azurepoc -ServerName "server-1416380557" -DatabaseName iisweb2 -ResourceGroupName "AzurePOC2RG"
-Set-AzureRmSqlDatabaseThreatDetectionPolicy -ResourceGroupName "AzurePOC2RG" -ServerName "server-1416380557" -DatabaseName iisweb2 -EmailAdmins $true -StorageAccountName azurepoc
-Set-AzureRmSqlDatabase -DatabaseName iisweb2 -ResourceGroupName "AzurePOC2RG" -ServerName "server-1416380557"
